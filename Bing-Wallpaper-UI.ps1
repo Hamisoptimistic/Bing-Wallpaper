250acf0b1aa4d7456ab874c9552454ca7231336c
HAMB <Hamisoptimistic@users.noreply.github.com>
2026-08-28 17:34:00 +0530

    112333221

--- Bing-Wallpaper-UI.ps1
+++ Bing-Wallpaper-UI.ps1
@@ -363,7 +363,7 @@ public static class BingWallpaperNative {
 }
 
 # Dynamically detect the installed executable's version; fallback to script version
-$script:appVersion = [Version]'1.0.180'
+$script:appVersion = [Version]'1.0.181'
 try {
     $currentProc = [System.Diagnostics.Process]::GetCurrentProcess().MainModule.FileName
     if ($currentProc -and $currentProc -notmatch '^(?i:powershell|pwsh)(?:\.exe)?$' -and (Test-Path -LiteralPath $currentProc)) {
@@ -4367,6 +4367,7 @@ $script:memTrimTimer.Start()
 
 $window.Show()
 [System.Windows.Threading.Dispatcher]::Run()
+
 
 
 
